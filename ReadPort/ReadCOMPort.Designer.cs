c21f3a934fd75249fabdbd9f03f7001fb281dc9c
NarendraOjha <NarendraOjha@DESKTOP-IHKB3U8>
2018-05-11 15:06:02 +1000

    Add project files.

---
+++ ReadPort/ReadCOMPort.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace ReadPort
+{
+    partial class ReadCOMPort
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // ReadCOMPort
+            // 
+            this.AutoLog = false;
+            this.CanShutdown = true;
+            this.ServiceName = "ReadCOMPort";
+
+        }
+
+        #endregion
+    }
+}
---
+++ ReadPort/ReadCOMPort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadPort
+{
+    public partial class ReadCOMPort : ServiceBase
+    {
+        public ReadCOMPort()
+        {
+            InitializeComponent();
+            EventLog.Log = "ReadCOMPort";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            EventLog.WriteEntry("Service started Successfully");
+
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("Service was stopped Successfully");
+        }
+
+        protected override void OnShutdown()
+        {
+            EventLog.WriteEntry("Service was Shutdown successfully");
+        }
+
+       
+    }
+}
